826cc13ffebfb7897dffa19ab8f07c7e0e44aa7a
Sacha Jungerman <jungerm2@illinois.edu>
2026-04-01 10:03:11 -0500

    update tests

--- tests/test_overlays.py
+++ tests/test_overlays.py
@@ -15,7 +15,7 @@ def section_duration(section):
 
 @pytest.snapshot_frames_comparison(last_frame=False)
 def test_basic_overlay(scene):
-    video = VideoMObject(pytest.get_test_clip(num_images=15, fps=15, shape=(512, 512))).stretch_to_keep_aspect()
+    video = VideoMObject(pytest.get_test_clip(num_images=15, fps=15)).stretch_to_keep_aspect()
     scene.play(OverlayVideo(video.scale(2.5)))
 
 
@@ -24,6 +24,7 @@ def test_overlay_with_different_fps(scene):
     video1 = VideoMObject(pytest.get_test_clip(num_images=10, fps=30))
     video2 = VideoMObject(pytest.get_test_clip(num_images=50, fps=150))
     video3 = VideoMObject(pytest.get_test_clip(num_images=5, fps=15))
+    video4 = VideoMObject(pytest.get_test_clip(num_images=9, fps=3))
     video1.next_to(video2, LEFT)
     video3.next_to(video2, RIGHT)
     scene.play(OverlayVideo(video1))
@@ -31,7 +32,12 @@ def test_overlay_with_different_fps(scene):
     scene.play(OverlayVideo(video2))
     scene.next_section()
     scene.play(OverlayVideo(video3))
+    scene.next_section()
+    scene.play(OverlayVideo(video4.scale(2)))
 
-    assert len(scene.renderer.file_writer.sections) == 3
-    d1, d2, d3 = [section_duration(section) for section in scene.renderer.file_writer.sections]
-    assert d1 == d2 == d3
+    assert len(scene.renderer.file_writer.sections) == 4
+    d1, d2, d3, d4 = [section_duration(section) for section in scene.renderer.file_writer.sections]
+    assert d1 == d2 == d3 == 0.33
+    assert d4 == 3.0
+
+
